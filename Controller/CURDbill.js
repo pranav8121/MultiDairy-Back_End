d7c81eaf06570e980b7cd6e9a0a6cd8fb647887a
pranav8121 <71267129+pranav8121@users.noreply.github.com>
2021-10-22 11:15:57 +0530

    Api Updated

--- Controller/CURDbill.js
+++ Controller/CURDbill.js
@@ -6,11 +6,11 @@ var { Bill } = require("../Model/bill");
 
 // FIND BILL
 router.get('/findBill/:UId/:inv_no/:No', (req, res) => {
-    Bill.find({ "inv_no": req.params.inv_no, "UId": req.params.UId,"No":req.params.No }, (err, doc) => {
-        if (doc.length>0) {
+    Bill.find({ "inv_no": req.params.inv_no, "UId": req.params.UId, "No": req.params.No }, (err, doc) => {
+        if (doc.length > 0) {
             res.json(doc);
         } else {
-            return res.status(501).json("Bill Not Made!!");
+            return res.status(501).json("Bill Not Generated!!");
         }
     });
 });
@@ -18,17 +18,21 @@ router.get('/findBill/:UId/:inv_no/:No', (req, res) => {
 
 router.post('/postBill', (req, res) => {
     var newBill = Bill({
-        No:req.body.No,
-        adv:req.body.adv ,
-        bank:req.body.bank ,
-        supply:req.body.supply ,
-        share:req.body.share ,
-        inv_no:req.body.inv_no ,
-        from:req.body.from ,
-        to:req.body.to ,
-        UId:req.body.UId,
+        Name: req.body.Name,
+        No: req.body.No,
+        adv: req.body.adv,
+        bank: req.body.bank,
+        supply: req.body.supply,
+        share: req.body.share,
+        inv_no: req.body.inv_no,
+        from: req.body.from,
+        to: req.body.to,
+        totalRate: req.body.totalRate,
+        cutting: req.body.cutting,
+        subAmount: req.body.subAmount,
+        UId: req.body.UId,
     });
-    newBill.save((err,doc)=>{
+    newBill.save((err, doc) => {
         if (!err) {
             res.json({ msg: "Bill Added Successfully", data: doc })
         }
--- Model/bill.js
+++ Model/bill.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose')
 
 var Bill = mongoose.model('Bill', {
-    No:{type :Number},
-   adv:{type :Number},
-   bank:{type :Number},
-   supply:{type :Number},
-   share:{type :Number},
-   inv_no:{type:String},
-   from:{type:String},
-   to:{type:String},
-   UId:{type:String}
+    Name: { type: String },
+    No: { type: Number },
+    totalRate: { type: Number },
+    cutting: { type: Number },
+    subAmount: { type: Number },
+    adv: { type: Number },
+    bank: { type: Number },
+    supply: { type: Number },
+    share: { type: Number },
+    inv_no: { type: String },
+    from: { type: String },
+    to: { type: String },
+    UId: { type: String }
 
 });
-module.exports = {Bill};
+module.exports = { Bill };
