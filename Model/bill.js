ecb79aa87d140ce34e5174c3475b2b070678d49e
pranav8121 <71267129+pranav8121@users.noreply.github.com>
2021-11-09 13:29:26 +0530

    Balance added

--- Model/bill.js
+++ Model/bill.js
@@ -14,7 +14,7 @@ var Bill = mongoose.model('Bill', {
     adv: { type: Number },
     bank: { type: Number },
     supply: { type: Number },
-    share: { type: Number },
+    balance: { type: Number },
     inv_no: { type: String },
     from: { type: String },
     to: { type: String },
